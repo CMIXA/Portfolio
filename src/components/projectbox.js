abe743905da3a782511b3babeb82dfb68557c50c
Chris Mixa <91347624+CMIXA@users.noreply.github.com>
2023-05-01 21:25:53 -0400

    Add files via upload

--- src/components/projectbox.js
+++ src/components/projectbox.js
@@ -3,7 +3,7 @@ import "../css/projectbox.css";
 
 const Projectbox = (props) => {
   return (
-    <div>
+    <div className="square-container">
       <a href={props.linkAddress}>
         <div class="square">
           <div class="text">{props.projectName}</div>
